67aaed0bebd592f10bc77c7e5849a00a706d50b9
OrangeFlavoredDerek <derekchan0407@gmail.com>
2022-04-16 22:04:36 +0800

    Initial Commit

---
+++ OC Note/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OC Note
+//
+//  Created by 陳峻琦 on 16/4/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
