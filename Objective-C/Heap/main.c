b9781f12b1c14161dd358215aaef02adf83bc8d9
OrangeFlavoredDerek <derekchan0407@gmail.com>
2022-05-24 14:39:47 +0800

    # update

--- Objective-C/Heap/main.c
+++ Objective-C/Heap/main.c
@@ -6,9 +6,32 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    float heightInMeters;
+    int weightInKilos;
+} Person;
+
+float bodyMassIndex(Person *p) {
+    return p->weightInKilos / (p->heightInMeters * p->heightInMeters);
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    //为一个Person 结构体分配内存
+    Person *mikey = (Person *)malloc(sizeof(Person));
+    
+    mikey->weightInKilos = 96;
+    mikey->heightInMeters = 1.7;
+    
+    float mikeyBMI = bodyMassIndex(mikey);
+    printf("mikey has a BMI of %f\n", mikeyBMI);
+    
+    //释放占用的内存，使之能够被重用
+    free(mikey);
+    
+    //将指针变量赋为空
+    mikey = NULL;
+    
     return 0;
 }
--- Objective-C/Pointer/main.c
+++ Objective-C/Pointer/main.c
@@ -6,9 +6,38 @@
 //
 
 #include <stdio.h>
+#include <math.h>
+
+void metersToFeetAndInches(double meters, unsigned int *ftPtr, double *inPtr) {
+    double rawFeet = meters * 3.281;
+    double integerPart;
+    //计算英尺
+    //unsigned int feet = (unsigned int)floor(rawFeet);
+    modf(rawFeet, &integerPart);
+    unsigned int feet = (unsigned int)integerPart;
+    
+    //将feet变量的值保存在传入的地址上
+    if (ftPtr) {
+        printf("Storing %u to the address %p\n", feet, ftPtr);
+        *ftPtr = feet;
+    }
+    
+    //计算英寸
+    double fractionalFoot = rawFeet - feet;
+    double inches = fractionalFoot * 12.0;
+    
+    if (inPtr) {
+        printf("Storing %.2f to the address %p\n", inches, inPtr);
+        *inPtr = inches;
+    }
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    double meters = 3.0;
+    unsigned int feet;
+    double inches;
+    
+    metersToFeetAndInches(meters, &feet, &inches);
+    printf("%.lf meters is equal to %d feet and %.lf inches.\n", meters, feet, inches);
     return 0;
 }
