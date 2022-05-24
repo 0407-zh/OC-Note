ad23db0843a86edb94d75453db27494dee141ac2
OrangeFlavoredDerek <derekchan0407@gmail.com>
2022-05-24 14:26:46 +0800

    # 补充C语言代码

---
+++ Objective-C/Pointer/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Pointer
+//
+//  Created by 陳峻琦 on 24/5/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
